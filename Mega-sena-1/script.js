deec899354478c3ff3c9fa81f658b615432554e0
Vinicius Bueno <79877937+Vinicius-a11y@users.noreply.github.com>
2023-02-13 20:42:46 -0300

    Mega Sena inicio do projetinho

--- Mega-sena-1/script.js
+++ Mega-sena-1/script.js
@@ -1,1 +1,27 @@
-console.log('teste');
+var state = {board: [], currentGame: [], saveGames: []};
+function start() {
+    addNumberToGame(1);
+    addNumberToGame(2);
+    addNumberToGame(3);
+    addNumberToGame(4);
+    addNumberToGame(5);
+    
+
+    console.log(state.currentGame);
+}
+
+function addNumberToGame(numberToAdd) {
+    if (numberToAdd < 1 || numberToAdd > 68) {
+        console.error('Numero invalido' , numberToAdd);
+        return;
+    }
+
+    if (state.currentGame.length >= 6){
+        console.error('o jogo ja esta completo.');
+        return;
+    }
+
+    state.currentGame.push(numberToAdd);
+}
+
+start();
